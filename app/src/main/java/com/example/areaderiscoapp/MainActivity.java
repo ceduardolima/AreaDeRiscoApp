2df8246e40d28dad2a49c7332bd1cb9aedd80c46
Matheus Diogo <matheusdiogoeng@gmail.com>
2022-05-30 22:40:55 -0300

    Tentativa de adicionar mapa na main

--- app/src/main/java/com/example/areaderiscoapp/MainActivity.java
+++ app/src/main/java/com/example/areaderiscoapp/MainActivity.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.gms.maps.SupportMapFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map_fragment);
     }
 }
