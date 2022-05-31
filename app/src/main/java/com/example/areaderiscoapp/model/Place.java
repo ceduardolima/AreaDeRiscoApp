a456c078221bbbd66cb70c0c8c4265db42b62090
Eduardo <ceduardolima2000@gmail.com>
2022-05-30 22:38:57 -0300

    Classe Place que ira armazenar cada local no mapa

---
+++ app/src/main/java/com/example/areaderiscoapp/model/Place.java
@@ -0,0 +1,51 @@
+package com.example.areaderiscoapp.model;
+
+import com.google.android.gms.maps.model.LatLng;
+
+class Place {
+    /* Classe usada para armazenar os dados de cada local */
+
+    String problem; // Armazena o tipo do problema
+    LatLng position; // Armazena a latitude e longitude
+    String address; // Armazena o endereço
+    double raiting;
+
+    public String getProblem() {
+        return problem;
+    }
+
+    public void setProblem(String problem) {
+        this.problem = problem;
+    }
+
+    public LatLng getPosition() {
+        return position;
+    }
+
+    public void setPosition(LatLng position) {
+        this.position = position;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public double getRaiting() {
+        return raiting;
+    }
+
+    public void setRaiting(double raiting) {
+        this.raiting = raiting;
+    }
+
+    public Place(String problem, LatLng position, String address, double raiting) {
+        this.problem = problem;
+        this.position = position;
+        this.address = address;
+        this.raiting = raiting;
+    }
+}
