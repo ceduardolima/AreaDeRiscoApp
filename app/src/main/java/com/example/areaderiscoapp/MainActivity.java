5bcf01f91b826196c37e4182e3b9f88149ae6b7e
Davi Pereira Rio <Davi.pereira.rio@gmail.com>
2022-06-05 01:13:46 -0300

    Adicionando Requerimento De Permissão Da Localização

--- app/src/main/java/com/example/areaderiscoapp/MainActivity.java
+++ app/src/main/java/com/example/areaderiscoapp/MainActivity.java
@@ -1,28 +1,48 @@
 package com.example.areaderiscoapp;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.app.DownloadManager;
+import android.content.IntentSender;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
 
 
 import com.example.areaderiscoapp.TSV_java.Chamado;
 import com.example.areaderiscoapp.TSV_java.TSVReader;
 import com.example.areaderiscoapp.model.map.Place;
 
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.ResolvableApiException;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.gun0912.tedpermission.PermissionListener;
+import com.gun0912.tedpermission.normal.TedPermission;
 
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private SupportMapFragment mapFragment;
     private ArrayList<Place> places;
     private ArrayList<Chamado> dataChamados;
+    int REQUEST_LOCATION = 88;
     Long downloadId1;
     Long downloadId2;
 
@@ -38,7 +58,93 @@ public class MainActivity extends AppCompatActivity {
         setTheme(R.style.Theme_Splash);
         initProcess();
         setContentView(R.layout.activity_main);
+
+        getLocation();
+
+        Button button = findViewById(R.id.location);
+        button.setOnClickListener(v -> {
+
+            //check permission
+            if(ActivityCompat.checkSelfPermission(MainActivity.this,Manifest.permission.ACCESS_FINE_LOCATION)
+            == PackageManager.PERMISSION_GRANTED){
+
+                //when permission is granted
+                Location();
+
+            } else {
+
+                //when permission is denied
+                ActivityCompat.requestPermissions(MainActivity.this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 23);
+            }
+        });
+
         init();
+    }
+
+    private void Location(){
+
+        LocationRequest request = LocationRequest.create();
+        request.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        request.setInterval(5000);
+        request.setFastestInterval(2000);
+
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
+                .addLocationRequest(request);
+        builder.setAlwaysShow(true);
+
+        Task<LocationSettingsResponse> result = LocationServices.getSettingsClient(getApplicationContext())
+                .checkLocationSettings(builder.build());
+        result.addOnCompleteListener(task -> {
+            try {
+                LocationSettingsResponse response = task.getResult(ApiException.class);
+
+                //do your task here with your location
+            } catch (ApiException e) {
+
+                switch (e.getStatusCode())
+                {
+                    case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                        try {
+                            ResolvableApiException resolvableApiException = (ResolvableApiException)e;
+                            resolvableApiException.startResolutionForResult(MainActivity.this,REQUEST_LOCATION);
+                        } catch (IntentSender.SendIntentException sendIntentException) {
+                        }
+                        break;
+                    //when device doesn't have location feature
+                    case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                        break;
+                }
+
+            }
+
+        });
+
+
+    }
+
+    private void getLocation() {
+
+        PermissionListener permissionlistener = new PermissionListener() {
+            @Override
+            public void onPermissionGranted() {
+                Toast.makeText(MainActivity.this, "Permission Granted", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onPermissionDenied(List<String> deniedPermissions) {
+                Toast.makeText(MainActivity.this, "Permission Denied\n" + deniedPermissions.toString(), Toast.LENGTH_SHORT).show();
+            }
+
+
+        };
+
+        TedPermission.create()
+                .setPermissionListener(permissionlistener)
+                .setDeniedMessage("If you reject permission,you can not use this service\n\nPlease turn on permissions at [Setting] > [Permission]")
+                .setPermissions(Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION)
+                .check();
+
     }
 
     private void init(){
